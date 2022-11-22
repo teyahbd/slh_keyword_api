3dd586f2d989d6c8720a82fa309416b509cfa983
teyahbd <teyahbrennendavies@gmail.com>
2022-11-22 12:51:02 +0000

    initialise git repo

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, abort, request
+from model import findKeywords
+import json
+
+app =  Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"Endpoints": {"/model": "accepts POST request containing keywords which returns related keywords"}}
+
+""" @app.route("/model", methods=['POST'])
+def model():
+    try:
+        words = request.json
+        return json.dumps({'keywords': findKeywords(words['positive'], words['negative'] )})
+    except:
+        abort(400) """
+
+@app.route("/*")
+def not_found():
+    abort(404)
+
+@app.errorhandler(400)
+def handle_400(e):
+    return 'Request object not formatted correctly', 400
---
+++ model.py
@@ -0,0 +1,9 @@
+from gensim.models import KeyedVectors
+from flask import abort
+
+""" model = KeyedVectors.load_word2vec_format('wiki-50d-word2vec.txt'
+, binary=False) """
+
+def findKeywords(likedKeywords, dislikedKeywords):
+    result = model.most_similar(positive=likedKeywords, negative=dislikedKeywords, topn=2)
+    return result
